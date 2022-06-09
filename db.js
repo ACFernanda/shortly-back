aba3906637bb4a2931d4f2ad538b51613ae2137e
acfernanda <acfernanda98@gmail.com>
2022-06-09 11:11:52 -0300

    Prepare for deploy

--- db.js
+++ db.js
@@ -5,12 +5,16 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const db = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: process.env.PASSWORD,
-  database: "shortly",
-});
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+if (process.env.MODE === "PROD") {
+  databaseConfig.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+
+const db = new Pool(databaseConfig);
 
 export default db;
